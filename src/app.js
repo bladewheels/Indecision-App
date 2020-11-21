14c295d6569256071c84b9fc5d6535d3bd806ee0
Michael Shields <michael.shields@iwellsite.com>
2020-11-20 22:19:41 -0700

    Third challenge, conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 console.log('App is running...');
 
-var app = { title: 'Indecision App', subTitle: 'This is some info' },
+var app = { 
+    title: 'Indecision App'
+    ,subTitle: 'This is some info'
+    ,options: [ 'One', 'Two' ]
+},
 template = (
     <div>
         <h1>{ app.title }</h1>
-        <p>{ app.subTitle }</p>
+        { app && app.subTitle && <p>{app.subTitle}</p> }
+        <p>{ (app && app.options && app.options.length > 0) ? 'Here are your options:' : 'No options' }</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
