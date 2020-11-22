8376af6efb7e698df3220e0c807397ecc7090195
Michael Shields <michael.shields@iwellsite.com>
2020-11-22 16:34:09 -0700

    Cleanup

--- src/playground/build-it-visibility.js
+++ src/playground/build-it-visibility.js
@@ -1,25 +1,3 @@
-// console.log('App is running...');
-
-// const app = { title: 'Visibility Toggle' },
-// toggle = () => {
-//     detailsShown = !detailsShown;
-//     render();
-// },
-// appRoot = document.getElementById('app'),
-// render = () => {
-//     const jsx = (
-//         <div>
-//             <h1>{ app.title }</h1>
-//             { detailsShown ? <button onClick={toggle}>Hide details</button> : <button onClick={toggle}>Show details</button> }
-//             { detailsShown && <p>Some details...</p> /* The && operator acts like an IF statement here */ }
-//         </div>
-//     );
-//     ReactDOM.render(jsx, appRoot);
-// };
-// let detailsShown = false;
-
-// render();
-
 class Visibility extends React.Component {
 
     constructor(props) {
