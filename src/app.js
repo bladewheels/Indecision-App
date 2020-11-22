d54265930d6bd8e65d1de37072bb8fb7a9f5864b
Michael Shields <michael.shields@iwellsite.com>
2020-11-21 17:49:03 -0700

    Button functions that manipulate the counter and re-render the JSX

--- src/app.js
+++ src/app.js
@@ -23,22 +23,44 @@ template = (
 appRoot = document.getElementById('app'),
 
 buttonIdForTemplateTwo = 'plus-one',
-addOne = () => { console.log('Added 1') },
-minusOne = () => { console.log('Subtracted 1') },
-reset = () => { console.log('Reset') },
-templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        {
-            // 'class' is a reserved word in JS so we must specify the 'className' attribute (of a DOM Element) in JSX instead.
-        }
-        <button id={buttonIdForTemplateTwo} className='btn' onClick={addOne}>+1</button>
-        <button id='plus-two' onClick={() => console.log('Added 2')}>+2</button>
-        <button id='plus-two' onClick={minusOne}>-1</button>
-        <button id='plus-two' onClick={reset}>Reset</button>
-    </div>
-);
+addOne = () => { 
+    count++;
+    renderCounterApp();
+},
+minusOne = () => { 
+    count--;
+    renderCounterApp();
+},
+reset = () => {
+    count = 0;
+    renderCounterApp();
+ }
+;
+
+const renderCounterApp = () => {
+
+    const template = (
+        <div>
+            <h1>Count: {count}</h1>
+            {
+                // 'class' is a reserved word in JS so we must specify the 'className' attribute (of a DOM Element) in JSX instead.
+            }
+            <button id={buttonIdForTemplateTwo} className='btn' onClick={addOne}>+1</button>
+            <button id='plus-two' onClick={() => {
+                count++; 
+                count++; 
+                renderCounterApp(); 
+            }}>+2</button>
+            <button id='plus-two' onClick={minusOne}>-1</button>
+            <button id='plus-two' onClick={reset}>Reset</button>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
+
 
 // console.log(templateTwo.props);
-ReactDOM.render(templateTwo, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
 // ReactDOM.render(template, appRoot);
+
+renderCounterApp();
