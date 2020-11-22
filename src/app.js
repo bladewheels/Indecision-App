3bc5fa6d6fb1e9906583d513a2ee54117bc1427f
Michael Shields <michael.shields@iwellsite.com>
2020-11-21 19:25:58 -0700

    Displaying the data model i.e. options

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 console.log('App is running...');
 
-let count = 0;
 const app = { 
     title: 'Indecision App'
     ,subTitle: 'Put your life in the hands of a computer'
@@ -19,6 +18,11 @@ resetOptions = () => {
     app.options = [];
     renderApp();
 },
+optionsAsList = () => {
+    app.options.map((option) => {
+        <li>option</li>
+    });
+},
 appRoot = document.getElementById('app'),
 renderApp = () => {
     const template = (
@@ -26,12 +30,15 @@ renderApp = () => {
             <h1>{ app.title }</h1>
             { app && app.subTitle && <p>{app.subTitle}</p> }
             <p>{ (app && app.options && app.options.length > 0) ? 'Here are your options:' : 'No options' }</p>
-            <p>{app.options.length}</p>
             <button onClick={resetOptions}>Reset Options</button>
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+            { /* Map over the options, using the index as the HTML key attribute */
+                app.options.map((option, index) => {
+                    return <li key={index}>{option}</li>;
+                })
+            }
             </ol>
+
             <form onSubmit={addOption}>
                 <input type="text" name="option"/>
                 <button>Add Option</button>
@@ -40,5 +47,6 @@ renderApp = () => {
     );
     ReactDOM.render(template, appRoot);
 };
+let count = 0;
 
 renderApp();
