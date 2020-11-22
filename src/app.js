2fb1d20899b6f80fe979175fad1bcbd562fc9d38
Michael Shields <michael.shields@iwellsite.com>
2020-11-22 13:06:27 -0700

    Introduce IndecisionApp component and it's children

--- src/app.js
+++ src/app.js
@@ -1,56 +1,66 @@
-console.log('App is running...');
-
-const app = { 
-    title: 'Indecision App'
-    ,subTitle: 'Put your life in the hands of a computer'
-    ,options: []
-},
-addOption = (event) => {
-    event.preventDefault();
-    const option = event.target.elements.option.value;
-    if (option) {
-        app.options.push(option);
-        event.target.elements.option.value = '';
-        renderApp();
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
     }
-},
-resetOptions = () => {
-    app.options = [];
-    renderApp();
-},
-pickOption = () => {
-    alert(app.options[Math.floor(Math.random() * app.options.length)]);
-},
-optionsAsList = () => {
-    app.options.map((option) => {
-        <li>option</li>
-    });
-},
-appRoot = document.getElementById('app'),
-renderApp = () => {
-    const template = (
-        <div>
-            <h1>{ app.title }</h1>
-            { app && app.subTitle && <p>{app.subTitle}</p> }
-            <p>{ (app && app.options && app.options.length > 0) ? 'Here are your options:' : 'No options' }</p>
-            <button disabled={0 === app.options.length} onClick={pickOption}>What should I do?</button>
-            <button onClick={resetOptions}>Reset Options</button>
-            <ol>
-            { /* Map over the options, using the index as the HTML key attribute */
-                app.options.map((option, index) => {
-                    return <li key={index}>{option}</li>;
-                })
-            }
-            </ol>
+}
 
-            <form onSubmit={addOption}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
-let count = 0;
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision App</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
 
-renderApp();
+class Action extends React.Component {
+    render() { return <button>What should I do?</button>; }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Your Options:</p>
+                <ol>
+                    <Option />
+                </ol>
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return <li>This is an option</li>;
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="option"/>
+                    <button>Add Option</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+// const jsx = (
+//     <div>
+//         <IndecisionApp />
+//     </div>
+// )
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ src/playground/jsx-indecision.js
@@ -0,0 +1,56 @@
+console.log('App is running...');
+
+const app = { 
+    title: 'Indecision App'
+    ,subTitle: 'Put your life in the hands of a computer'
+    ,options: []
+},
+addOption = (event) => {
+    event.preventDefault();
+    const option = event.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        event.target.elements.option.value = '';
+        renderApp();
+    }
+},
+resetOptions = () => {
+    app.options = [];
+    renderApp();
+},
+pickOption = () => {
+    alert(app.options[Math.floor(Math.random() * app.options.length)]);
+},
+optionsAsList = () => {
+    app.options.map((option) => {
+        <li>option</li>
+    });
+},
+appRoot = document.getElementById('app'),
+renderApp = () => {
+    const template = (
+        <div>
+            <h1>{ app.title }</h1>
+            { app && app.subTitle && <p>{app.subTitle}</p> }
+            <p>{ (app && app.options && app.options.length > 0) ? 'Here are your options:' : 'No options' }</p>
+            <button disabled={0 === app.options.length} onClick={pickOption}>What should I do?</button>
+            <button onClick={resetOptions}>Reset Options</button>
+            <ol>
+            { /* Map over the options, using the index as the HTML key attribute */
+                app.options.map((option, index) => {
+                    return <li key={index}>{option}</li>;
+                })
+            }
+            </ol>
+
+            <form onSubmit={addOption}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+let count = 0;
+
+renderApp();
