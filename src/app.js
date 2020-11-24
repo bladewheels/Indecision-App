93ae2cc9ec05ac51907ea715e7b7773c8563ff12
Michael Shields <michael.shields@iwellsite.com>
2020-11-24 12:35:04 -0700

    Preparing for 3rd-party component inclusion, exploring implicit props.children

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
+import { checkPropTypes } from 'prop-types';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp subtitle={'Put your life in the hands of a computer'} />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>header</p>
+            {props.content}
+            {props.children}
+            <p>footer</p>
+        </div>
+    );
+};
+
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>This is my page, via props.content</p>
+    </div>
+);
+
+// ReactDOM.render(<IndecisionApp subtitle={'Put your life in the hands of a computer'} />, document.getElementById('app'));
+// ReactDOM.render(<Layout content={template}/>, document.getElementById('app'));
+ReactDOM.render((
+    <Layout content={template}>
+        <p>this is inline, via props.children</p>
+    </Layout>)
+    , document.getElementById('app'));
