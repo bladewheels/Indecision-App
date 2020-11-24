94a4fae1f636af1945161468b091849d3daa3ab0
Michael Shields <michael.shields@iwellsite.com>
2020-11-23 17:02:16 -0700

    Cleanup

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@
 // console.log('5-6=', subtract(5,6));
 
 import isSeniorInAlberta, { isAdult, canDrinkAlcohol } from './person.js' ;
-// console.log(`If your age is 17 then you are ${((isAdult(17))? '': 'not ')}considered an adult in Alberta.`);
-// console.log(`If your age is 18 then you are ${((isAdult(18))? '': 'not ')}considered an adult in Alberta.`);
-// console.log(`If your age is 17 then you can ${((canDrinkAlcohol(17))? '': 'not ')}drink alcohol in Alberta.`);
-// console.log(`If your age is 18 then you can ${((canDrinkAlcohol(18))? '': 'not ')}drink alcohol in Alberta.`);
-console.log(`If your age is 64 then you are ${((isSeniorInAlberta(64))? '': 'not ')}considered a senior citizen in Alberta.`);
-console.log(`If your age is 65 then you are ${((isSeniorInAlberta(65))? '': 'not ')}considered a senior citizen in Alberta.`);
+// console.log(`If your age is 17 then you are ${((isAdult(17)) ? '': 'not')} considered an adult in Alberta.`);
+// console.log(`If your age is 18 then you are ${((isAdult(18)) ? '': 'not')} considered an adult in Alberta.`);
+// console.log(`If your age is 17 then you can ${((canDrinkAlcohol(17)) ? '': 'not ')}drink alcohol in Alberta.`);
+// console.log(`If your age is 18 then you can ${((canDrinkAlcohol(18)) ? '': 'not ')}drink alcohol in Alberta.`);
+console.log(`If your age is 64 then you are ${((isSeniorInAlberta(64)) ? '': 'not')} considered a senior citizen in Alberta.`);
+console.log(`If your age is 65 then you are ${((isSeniorInAlberta(65)) ? '': 'not')} considered a senior citizen in Alberta.`);
