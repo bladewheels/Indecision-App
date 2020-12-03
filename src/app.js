e8cc4cf7a110cae7d802d40cb0b1b7610eb82714
Michael Shields <michael.shields@iwellsite.com>
2020-12-03 16:41:26 -0700

    Start adding styles with SCSS

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp subtitle={'Put your life in the hands of a computer'} />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,16 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }]
+        },
+        {
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ],
+            test: /\.scss$/,
+        }
+        ]
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
